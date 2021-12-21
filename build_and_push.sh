a518b11029b6f044a5c08a314be5ce078c9947fa
tchekjunior <tchekjunior@gmail.com>
2021-12-21 13:37:38 -0500

    add example

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Run this once: docker buildx create --use --name build --node build --driver-opt network=host
-docker buildx build --platform linux/arm/v7,linux/arm64/v8,linux/amd64 -t tchekjunior/pihole-unbound:`cat VERSION` --push .
-docker buildx build --platform linux/arm/v7,linux/arm64/v8,linux/amd64 -t tchekjunior/pihole-unbound:latest --push .
+docker buildx build --platform linux/arm/v7,linux/arm64/v8,linux/amd64 -t tchekjunior/pihole-unbound-docker:2021.12.20 --push .
+docker buildx build --platform linux/arm/v7,linux/arm64/v8,linux/amd64 -t tchekjunior/pihole-unbound-docker:latest --push .
 
 
